f0f639d9d9fc2c67768781da91e88d03b9ca77a1
Yailama <adakopyan@gmail.com>
2017-03-23 10:31:21 +0400

    New test uploaded
    New fixed test uploaded

---
+++ tests/test-script.R
@@ -0,0 +1,7 @@
+library(testthat)
+
+expect_that(make_filename(2015), is_a("character"))
+expect_match(make_filename(2015), "accident_2015.csv.bz2")
+
+expect_that(fars_read(2015), is_a("character"))
+expect_error()
