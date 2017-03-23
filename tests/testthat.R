b5e38fc5dd235748c93cda4d32c05454d0761cb1
Yailama <adakopyan@gmail.com>
2017-03-23 19:57:53 +0300

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,5 @@ library(testthat)
 library(assignment4)
 
 test_check("assignment4")
+test_file("test-script.R")
 
-expect_that(make_filename(2015), is_a("character"))
-expect_match(make_filename(2015), "accident_2015.csv.bz2")
