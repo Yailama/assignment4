daddf1b1b77586f48d088a8c7e912dc5a4faab3d
Yailama <adakopyan@gmail.com>
2017-03-23 15:44:58 +0300

    Update test-script.R

--- tests/test-script.R
+++ tests/test-script.R
@@ -3,5 +3,3 @@ library(assignment4)
 
 expect_that(make_filename(2015), is_a("character"))
 expect_match(make_filename(2015), "accident_2015.csv.bz2")
-
-expect_that(fars_read(2015), is_a("character"))
